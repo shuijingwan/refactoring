4d52a277cada355f756fcbb6c2cc78fe872c8a40
wangqiang <958104607@qq.com>
2022-11-16 21:56:38 +0800

    分离到两个文件（和两个阶段），模块化初步实现

---
+++ 1/statement.js
@@ -0,0 +1,56 @@
+import createStatementData from './createStatementData.js';
+
+function renderPlainText(data, plays) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
+    result += `You earned ${data.totalVolumeCredits} credits\n`;
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD", minimumFractionDigits: 2
+    }).format(aNumber / 100);
+}
+
+function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
+}
+
+let invoice = {
+    "customer": "BigCo",
+    "performances": [
+        {
+            "playID": "hamlet",
+            "audience": 55
+        },
+        {
+            "playID": "as-like",
+            "audience": 35
+        },
+        {
+            "playID": "othello",
+            "audience": 40
+        }
+    ]
+};
+
+let plays = {
+    "hamlet": {
+        "name": "Hamlet",
+        "type": "tragedy"
+    },
+    "as-like": {
+        "name": "As You Like It",
+        "type": "comedy"
+    },
+    "othello": {
+        "name": "Othello",
+        "type": "tragedy"
+    }
+};
+
+console.log(statement(invoice, plays));
