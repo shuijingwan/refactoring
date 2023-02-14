87a08110ea13bb8f69a7ae52f6adb3cfe522c997
wangqiang <958104607@qq.com>
2023-02-14 21:11:31 +0800

    把常量(测试夹具)提取到外层作用域里

--- test/province.js
+++ test/province.js
@@ -16,12 +16,11 @@ function sampleProvinceData() {
 
 var expect = chai.expect;
 describe('province', function() {
+    const asia = new Province(sampleProvinceData()); // DON'T DO THIS
     it('shortfall', function() {
-        const asia = new Province(sampleProvinceData());
         expect(asia.shortfall).equal(5);
     });
     it('profit', function() {
-        const asia = new Province(sampleProvinceData());
         expect(asia.profit).equal(230);
     });
 });
