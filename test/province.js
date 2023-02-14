9b40536bae476ff8dde75f88b7cd07857d537d19
wangqiang <958104607@qq.com>
2023-02-14 21:30:46 +0800

    Mocha把这也当作测试失败（failure），但多数测试框架会把它当作一个错误（error），并与正常的测试失败区分开

--- test/province.js
+++ test/province.js
@@ -42,7 +42,7 @@ describe('province', function () {
         expect(asia.shortfall).equal(-26);
         expect(asia.profit).equal(-10);
     });
-    it('empty string demand', function() {
+    it('empty string demand', function () {
         asia.demand = "";
         expect(asia.shortfall).NaN;
         expect(asia.profit).NaN;
@@ -65,5 +65,18 @@ describe('no producers', function () {
     });
     it('profit', function () {
         expect(noProducers.profit).equal(0);
+    });
+});
+
+describe('string for producers', function () {
+    it('', function () {
+        const data = {
+            name: "String producers",
+            producers: "",
+            demand: 30,
+            price: 20
+        };
+        const prov = new Province(data);
+        expect(prov.shortfall).equal(0);
     });
 });
