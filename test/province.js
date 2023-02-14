44ab6399f3e14436c3556e17945d6b3a54b06c19
wangqiang <958104607@qq.com>
2023-02-14 21:24:10 +0800

    考虑可能出错的边界条件，把测试火力集中在那儿。

--- test/province.js
+++ test/province.js
@@ -15,6 +15,7 @@ function sampleProvinceData() {
 }
 
 var expect = chai.expect;
+
 describe('province', function () {
     let asia;
     beforeEach(function () {
@@ -26,9 +27,38 @@ describe('province', function () {
     it('profit', function () {
         expect(asia.profit).equal(230);
     });
-    it('change production', function() {
+    it('change production', function () {
         asia.producers[0].production = 20;
         expect(asia.shortfall).equal(-6);
         expect(asia.profit).equal(292);
+    });
+    it('zero demand', function () {
+        asia.demand = 0;
+        expect(asia.shortfall).equal(-25);
+        expect(asia.profit).equal(0);
+    });
+    it('negative demand', function () {
+        asia.demand = -1;
+        expect(asia.shortfall).equal(-26);
+        expect(asia.profit).equal(-10);
+    });
+});
+
+describe('no producers', function () {
+    let noProducers;
+    beforeEach(function () {
+        const data = {
+            name: "No proudcers",
+            producers: [],
+            demand: 30,
+            price: 20
+        };
+        noProducers = new Province(data);
+    });
+    it('shortfall', function () {
+        expect(noProducers.shortfall).equal(30);
+    });
+    it('profit', function () {
+        expect(noProducers.profit).equal(0);
     });
 });
