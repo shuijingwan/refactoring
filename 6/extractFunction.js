3ff5b76384fdd3bd73fa45986a708db4d8e1b2af
wangqiang <958104607@qq.com>
2023-02-28 21:34:44 +0800

    范例：无局部变量

--- 6/extractFunction.js
+++ 6/extractFunction.js
@@ -17,14 +17,25 @@ let invoice = {
 };
 
 function printOwing(invoice) {
-    printBanner();
-    let outstanding = calculateOutstanding();
-    printDetails(outstanding);
+    let outstanding = 0;
 
-    function printDetails(outstanding) {
-        console.log(`name: ${invoice.customer}`);
-        console.log(`amount: ${outstanding}`);
+    console.log("***********************");
+    console.log("**** Customer Owes ****");
+    console.log("***********************");
+
+    // calculate outstanding
+    for (const o of invoice.orders) {
+        outstanding += o.amount;
     }
+
+    // record due date
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+    //print details
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
 }
 
 printOwing(invoice);
