d8e95119468839661eecdabd780f8ac26c11a047
wangqiang <958104607@qq.com>
2023-03-02 21:09:58 +0800

    内联函数（Inline Function）

--- 6/inlineFunction.js
+++ 6/inlineFunction.js
@@ -1,7 +1,3 @@
 function getRating(driver) {
-    return moreThanFiveLateDeliveries(driver) ? 2 : 1;
-}
-
-function moreThanFiveLateDeliveries(driver) {
-    return driver.numberOfLateDeliveries > 5;
+    return (driver.numberOfLateDeliveries > 5) ? 2 : 1;
 }
