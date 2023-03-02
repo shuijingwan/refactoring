fb306d47cd8cb6a7d0ab0c36d33de57584427b94
wangqiang <958104607@qq.com>
2023-03-02 21:22:06 +0800

    继续处理后面的代码行，直到完成整个重构

--- 6/inlineFunction.js
+++ 6/inlineFunction.js
@@ -5,13 +5,6 @@ function getRating(driver) {
 function reportLines(aCustomer) {
     const lines = [];
     lines.push(["name", aCustomer.name]);
-    gatherCustomerData(lines, aCustomer);
+    lines.push(["location", aCustomer.location]);
     return lines;
-}
-
-function gatherCustomerData(out, aCustomer) {
-
-    out.push(["name", aCustomer.name]);
-
-    out.push(["location", aCustomer.location]);
 }
