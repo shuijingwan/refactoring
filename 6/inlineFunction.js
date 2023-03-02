58d48c0ec8ba13c43ed9671469e7bc346cd462a4
wangqiang <958104607@qq.com>
2023-03-02 21:20:22 +0800

    对第一行代码使用搬移语句到调用者

--- 6/inlineFunction.js
+++ 6/inlineFunction.js
@@ -4,11 +4,14 @@ function getRating(driver) {
 
 function reportLines(aCustomer) {
     const lines = [];
+    lines.push(["name", aCustomer.name]);
     gatherCustomerData(lines, aCustomer);
     return lines;
 }
 
 function gatherCustomerData(out, aCustomer) {
+
     out.push(["name", aCustomer.name]);
+
     out.push(["location", aCustomer.location]);
 }
