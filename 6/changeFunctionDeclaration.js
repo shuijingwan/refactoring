ec0fb3a124ddee992a16714e81848968c296a4b9
wangqiang <958104607@qq.com>
2023-03-06 21:57:27 +0800

    利用JavaScript的语言特性先应用引入断言（302），确保调用方一定会用到这个新参数

--- 6/changeFunctionDeclaration.js
+++ 6/changeFunctionDeclaration.js
@@ -12,6 +12,7 @@ class Book {
     }
 
     zz_addReservation(customer, isPriority) {
+        assert(isPriority === true || isPriority === false);
         this._reservations.push(customer);
     }
 }
