616324d18f1d8e1f3df7a05b870140d7eb96e534
wangqiang <958104607@qq.com>
2023-03-06 21:51:37 +0800

    先给新函数起一个容易搜索的临时名字。

--- 6/changeFunctionDeclaration.js
+++ 6/changeFunctionDeclaration.js
@@ -8,6 +8,10 @@ function circumference(radius) {
 
 class Book {
     addReservation(customer) {
+        this.zz_addReservation(customer);
+    }
+
+    zz_addReservation(customer) {
         this._reservations.push(customer);
     }
 }
