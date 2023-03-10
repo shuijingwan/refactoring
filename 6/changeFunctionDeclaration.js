5ebd966804896bde25459d1446a3445dabb7051c
wangqiang <958104607@qq.com>
2023-03-10 21:36:33 +0800

    把旧函数的调用处改为调用新函数

--- 6/changeFunctionDeclaration.js
+++ 6/changeFunctionDeclaration.js
@@ -25,4 +25,4 @@ function xxNEWinNewEngland(stateCode) {
     return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
 }
 
-const newEnglanders = someCustomers.filter(c => inNewEngland(c));
+const newEnglanders = someCustomers.filter(c => xxNEWinNewEngland(c.address.state));
