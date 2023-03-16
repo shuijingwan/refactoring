826a536daba207ed66a6d81b4b50ad30e2f109f2
wangqiang <958104607@qq.com>
2023-03-16 21:39:24 +0800

    通过封装记录阻止对数据的修改

--- 6/6.6/defaultOwner.js
+++ 6/6.6/defaultOwner.js
@@ -1,9 +1,26 @@
 let defaultOwnerData = {firstName: "Martin", lastName: "Fowler"};
 
 export function defaultOwner() {
-    return Object.assign({}, defaultOwnerData);
+    return new Person(defaultOwnerData);
 }
 
 export function setDefaultOwner(arg) {
     defaultOwnerData = arg;
+}
+
+class Person {
+    constructor(data) {
+        this._lastName = data.lastName;
+        this._firstName = data.firstName
+    }
+
+    get lastName() {
+        return this._lastName;
+    }
+
+    get firstName() {
+        return this._firstName;
+    }
+
+// and so on for other properties
 }
