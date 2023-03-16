8b90d88a0a7f39ec7dcd448217e88aa214d93f6e
wangqiang <958104607@qq.com>
2023-03-16 21:28:50 +0800

    将变量和访问函数搬移到单独一个文件中

---
+++ 6/6.6/defaultOwner.js
@@ -0,0 +1,9 @@
+let defaultOwner = {firstName: "Martin", lastName: "Fowler"};
+
+export function getDefaultOwner() {
+    return defaultOwner;
+}
+
+export function setDefaultOwner(arg) {
+    defaultOwner = arg;
+}
