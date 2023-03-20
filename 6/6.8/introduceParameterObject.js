384fc848f3efeb20113fb3dd75a71991e5cfe867
wangqiang <958104607@qq.com>
2023-03-20 21:10:40 +0800

    引入参数对象（Introduce Parameter Object）

---
+++ 6/6.8/introduceParameterObject.js
@@ -0,0 +1,11 @@
+function amountInvoiced(startDate, endDate) {
+    //...
+}
+
+function amountReceived(startDate, endDate) {
+    //...
+}
+
+function amountOverdue(startDate, endDate) {
+    //...
+}
