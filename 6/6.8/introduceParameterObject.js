d8954b5a2180b9b747c97acde3cd980996fd1200
wangqiang <958104607@qq.com>
2023-03-20 21:23:41 +0800

    运用改变函数声明（124），把新的对象作为参数传给readingsOutsideRange

--- 6/6.8/introduceParameterObject.js
+++ 6/6.8/introduceParameterObject.js
@@ -21,7 +21,7 @@ const station = {
     ]
 };
 
-function readingsOutsideRange(station, min, max) {
+function readingsOutsideRange(station, min, max, range) {
     return station.readings
         .filter(r => r.temp < min || r.temp > max);
 }
