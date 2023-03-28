8624ec421e78eb5f9ee4e10b31ec6b624adff3f0
wangqiang <958104607@qq.com>
2023-03-28 20:51:05 +0800

    运用提炼函数（106）将计算应税费用（taxable charge）的逻辑提炼成函数

--- 6/6.9/client2.js
+++ 6/6.9/client2.js
@@ -1,3 +1,7 @@
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-const taxableCharge = Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+const taxableCharge = taxableChargeFn(aReading);
+
+function taxableChargeFn(aReading) {
+    return Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+}
