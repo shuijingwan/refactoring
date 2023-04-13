13bc99b87fef840aa50cba7536d4947b4f264ddc
wangqiang <958104607@qq.com>
2023-04-13 21:35:58 +0800

    函数组合成变换（Combine Functions into Transform）

--- 6/6.10/combineFunctionsIntoTransform.js
+++ 6/6.10/combineFunctionsIntoTransform.js
@@ -1,7 +1,6 @@
-function base(aReading) {
-    //...
-}
-
-function taxableCharge(aReading) {
-    //...
+function enrichReading(argReading) {
+    const aReading = _.cloneDeep(argReading);
+    aReading.baseCharge = base(aReading);
+    aReading.taxableCharge = taxableCharge(aReading);
+    return aReading;
 }
