908e0db0dc30b94acd0c25478ab597701c14c8b2
wangqiang <958104607@qq.com>
2023-04-17 21:49:44 +0800

    把计算逻辑搬移到变换函数中

--- 6/6.10/combineFunctionsIntoTransform.js
+++ 6/6.10/combineFunctionsIntoTransform.js
@@ -7,6 +7,7 @@ function calculateBaseCharge(aReading) {
 function enrichReading(original) {
     const result = _.cloneDeep(original);
     result.baseCharge = calculateBaseCharge(result);
+    result.taxableCharge = Math.max(0, result.baseCharge - taxThreshold(result.year));
     return result;
 }
 
