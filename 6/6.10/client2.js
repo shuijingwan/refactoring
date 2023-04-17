b75ff97b5a648c2c5d30d7aa17aac2d338de06e5
wangqiang <958104607@qq.com>
2023-04-17 21:50:46 +0800

    修改使用方代码，让它使用新添的字段

--- 6/6.10/client2.js
+++ 6/6.10/client2.js
@@ -1,3 +1,3 @@
 const rawReading = acquireReading();
 const aReading = enrichReading(rawReading);
-const taxableCharge = Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+const taxableCharge = aReading.taxableCharge;
