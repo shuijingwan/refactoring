2f722a1aed794a037606cb29651c5a561e81b0e9
wangqiang <958104607@qq.com>
2023-04-17 21:47:07 +0800

    基本费用的计算逻辑马上就可以改用变换得到的新字段代替

--- 6/6.10/client2.js
+++ 6/6.10/client2.js
@@ -1,4 +1,4 @@
 const rawReading = acquireReading();
 const aReading = enrichReading(rawReading);
-const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
+const base = aReading.baseCharge;
 const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
