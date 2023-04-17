3e0b8660acf128b5b447d434755e1122f6736846
wangqiang <958104607@qq.com>
2023-04-17 21:38:41 +0800

    修改客户端代码，令其改用增强后的字段

--- 6/6.10/client3.js
+++ 6/6.10/client3.js
@@ -1,13 +1,3 @@
 const rawReading = acquireReading();
 const aReading = enrichReading(rawReading);
-const basicChargeAmount = calculateBaseCharge(aReading);
-
-function calculateBaseCharge(aReading) {
-    return baseRate(aReading.month, aReading.year) * aReading.quantity;
-}
-
-function enrichReading(original) {
-    const result = _.cloneDeep(original);
-    result.baseCharge = calculateBaseCharge(result);
-    return result;
-}
+const basicChargeAmount = aReading.baseCharge;
--- 6/6.10/combineFunctionsIntoTransform.js
+++ 6/6.10/combineFunctionsIntoTransform.js
@@ -1,8 +1,11 @@
 reading = {customer: "ivan", quantity: 10, month: 5, year: 2017};
 
-function enrichReading(argReading) {
-    const aReading = _.cloneDeep(argReading);
-    aReading.baseCharge = base(aReading);
-    aReading.taxableCharge = taxableCharge(aReading);
-    return aReading;
+function calculateBaseCharge(aReading) {
+    return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
+function enrichReading(original) {
+    const result = _.cloneDeep(original);
+    result.baseCharge = calculateBaseCharge(result);
+    return result;
 }
