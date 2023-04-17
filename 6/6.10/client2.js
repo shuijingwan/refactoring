042fffcce585ede04622300aa6ab9bcc640ed8e2
wangqiang <958104607@qq.com>
2023-04-17 21:45:10 +0800

    转头去看“计算应税费用”的逻辑。第一步是把变换函数用起来

--- 6/6.10/client2.js
+++ 6/6.10/client2.js
@@ -1,3 +1,4 @@
-const aReading = acquireReading();
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
 const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
 const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
