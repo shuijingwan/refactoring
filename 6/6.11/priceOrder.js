f9e1be9fc9213154b4bbf06143db40edeffb4270
wangqiang <958104607@qq.com>
2023-04-20 21:49:42 +0800

    把第一阶段代码提炼成独立的函数，令其返回这份数据

--- 6/6.11/priceOrder.js
+++ 6/6.11/priceOrder.js
@@ -1,13 +1,17 @@
 function priceOrder(product, quantity, shippingMethod) {
-    const basePrice = product.basePrice * quantity;
-    const discount = Math.max(quantity - product.discountThreshold, 0)
-        * product.basePrice * product.discountRate;
-    const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
-    const price = applyShipping(priceData, shippingMethod, discount);
+    const priceData = calculatePricingData(product, quantity);
+    const price = applyShipping(priceData, shippingMethod);
     return price;
 }
 
-function applyShipping(priceData, shippingMethod, discount) {
+function calculatePricingData(product, quantity) {
+    const basePrice = product.basePrice * quantity;
+    const discount = Math.max(quantity - product.discountThreshold, 0)
+        * product.basePrice * product.discountRate;
+    return {basePrice: basePrice, quantity: quantity, discount: discount};
+}
+
+function applyShipping(priceData, shippingMethod) {
     const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = priceData.quantity * shippingPerCase;
