b0fea3093f53e7a6ab02a2c6de1bd3295caf8e81
wangqiang <958104607@qq.com>
2023-04-20 21:48:02 +0800

    对discount参数我也如法炮制

--- 6/6.11/priceOrder.js
+++ 6/6.11/priceOrder.js
@@ -2,15 +2,15 @@ function priceOrder(product, quantity, shippingMethod) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
-    const priceData = {basePrice: basePrice, quantity: quantity};
-    const price = applyShipping(priceData, shippingMethod, quantity, discount);
+    const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
+    const price = applyShipping(priceData, shippingMethod, discount);
     return price;
 }
 
-function applyShipping(priceData, shippingMethod, quantity, discount) {
+function applyShipping(priceData, shippingMethod, discount) {
     const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = priceData.quantity * shippingPerCase;
-    const price = priceData.basePrice - discount + shippingCost;
+    const price = priceData.basePrice - priceData.discount + shippingCost;
     return price;
 }
