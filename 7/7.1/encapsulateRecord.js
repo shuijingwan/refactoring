d4fc2c2b8f26af3a5ee82ec50a91642700957af3
wangqiang <958104607@qq.com>
2023-04-27 21:20:17 +0800

    封装记录意味着，仅仅替换变量还不够，我还想控制它的使用方式。我可以用类来替换记录，从而达到这一目的

--- 7/7.1/encapsulateRecord.js
+++ 7/7.1/encapsulateRecord.js
@@ -13,23 +13,6 @@ getRawDataOfOrganization().name = newName;
 
 class Organization {
     constructor(data) {
-        this._name = data.name;
-        this._country = data.country;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    set name(arg) {
-        this._name = arg;
-    }
-
-    get country() {
-        return this._country;
-    }
-
-    set country(arg) {
-        this._country = arg;
+        this._data = data;
     }
 }
