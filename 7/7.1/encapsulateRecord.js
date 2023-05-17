fe589bc6bfdb74711c4f0bb097dd123ee32d9b44
wangqiang <958104607@qq.com>
2023-05-17 21:30:40 +0800

    这里有一个嵌套层级更深的数据：它是一组顾客信息的集合，保存在散列映射中，并通过顾客ID进行索引

--- 7/7.1/encapsulateRecord.js
+++ 7/7.1/encapsulateRecord.js
@@ -30,3 +30,24 @@ class Organization {
         this._country = aCountryCode;
     }
 }
+
+// "1920": {
+//     name: "martin",
+//         id: "1920",
+//         usages: {
+//             "2016": {
+//                 "1": 50,
+//                 "2": 55,
+//                 // remaining months of the year
+//             },
+//             "2015": {
+//                 "1": 70,
+//                 "2": 63,
+//                 // remaining months of the year
+//             }
+//         }
+//     },
+// "38673": {
+//     name: "neal",
+//         id: "38673",
+// more customers in a similar form
