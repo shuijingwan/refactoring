1a2935b443a5f5bf2dc23627f287cdc6899e65c6
wangqiang <958104607@qq.com>
2023-06-06 21:30:19 +0800

    对这样的数据施行封装，第一步仍是封装变量（132）

--- 7/7.1/encapsulateRecord.js
+++ 7/7.1/encapsulateRecord.js
@@ -52,10 +52,18 @@ class Organization {
 //         id: "38673",
 // more customers in a similar form
 
-customerData[customerID].usages[year][month] = amount;
+getRawDataOfCustomers()[customerID].usages[year][month] = amount;
 
-function compareUsage (customerID, laterYear, month) {
-    const later = customerData[customerID].usages[laterYear][month];
-    const earlier = customerData[customerID].usages[laterYear - 1][month];
+function compareUsage(customerID, laterYear, month) {
+    const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
+    const earlier = getRawDataOfCustomers()[customerID].usages[laterYear - 1][month];
     return {laterAmount: later, change: later - earlier};
+}
+
+function getRawDataOfCustomers() {
+    return customerData;
+}
+
+function setRawDataOfCustomers(arg) {
+    customerData = arg;
 }
