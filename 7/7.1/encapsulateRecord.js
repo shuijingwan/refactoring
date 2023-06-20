a1cb0a03791f8912d03e3ef63f7af2e14b6282fe
wangqiang <958104607@qq.com>
2023-06-20 20:47:58 +0800

    最简单的应对办法是返回原始数据的一份副本，这可以用到我前面写的rawData方法。

--- 7/7.1/encapsulateRecord.js
+++ 7/7.1/encapsulateRecord.js
@@ -55,8 +55,8 @@ class Organization {
 getCustomerData().setUsage(customerID, year, month, amount);
 
 function compareUsage(customerID, laterYear, month) {
-    const later = getCustomerData().usage(customerID, laterYear, month);
-    const earlier = getCustomerData().usage(customerID, laterYear - 1, month);
+    const later = getCustomerData().rawData[customerID].usages[laterYear][month];
+    const earlier = getCustomerData().rawData[customerID].usages[laterYear - 1][month];
     return {laterAmount: later, change: later - earlier};
 }
 
