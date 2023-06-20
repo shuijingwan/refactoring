f7e6f2c5318d1d3d15b050b878f8c6bfc7e037b9
wangqiang <958104607@qq.com>
2023-06-20 21:10:47 +0800

    封装集合（Encapsulate Collection）

---
+++ 7/7.2/encapsulateCollection.js
@@ -0,0 +1,21 @@
+// class Person {
+//     get courses() {
+//         return this._courses;
+//     }
+//
+//     set courses(aList) {
+//         this._courses = aList;
+//     }
+// }
+
+class Person {
+    get courses() {
+        return this._courses.slice();
+    }
+
+    addCourse(aCourse) {
+    }
+
+    removeCourse(aCourse) {
+    }
+}
