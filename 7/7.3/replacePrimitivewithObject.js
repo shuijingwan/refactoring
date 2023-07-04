477944d64695c9bcb0e4d690b07c445171c25ad3
wangqiang <958104607@qq.com>
2023-07-04 21:14:45 +0800

    客户端代码有些地方是这么用它的

--- 7/7.3/replacePrimitivewithObject.js
+++ 7/7.3/replacePrimitivewithObject.js
@@ -1,4 +1,13 @@
-orders.filter(o => "high" === o.priority
-    || "rush" === o.priority);
+// orders.filter(o => "high" === o.priority || "rush" === o.priority);
 
 orders.filter(o => o.priority.higherThan(new Priority("normal")))
+
+highPriorityCount = orders.filter(o => "high" === o.priority || "rush" === o.priority).length;
+
+class Order {
+    constructor(data) {
+        this.priority = data.priority;
+        // more initialization
+    }
+}
+
