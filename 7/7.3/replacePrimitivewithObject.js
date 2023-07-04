dde3c21ba55fa5d55a2c2d16578ad019e144110f
wangqiang <958104607@qq.com>
2023-07-04 21:26:35 +0800

    然后我要修改访问函数，使其用上新创建的类

--- 7/7.3/replacePrimitivewithObject.js
+++ 7/7.3/replacePrimitivewithObject.js
@@ -11,11 +11,20 @@ class Order {
     }
 
     get priority() {
-        return this._priority;
+        return this._priority.toString();
     }
 
     set priority(aString) {
-        this._priority = aString;
+        this._priority = new Priority(aString);
     }
 }
 
+class Priority {
+    constructor(value) {
+        this._value = value;
+    }
+
+    toString() {
+        return this._value;
+    }
+}
