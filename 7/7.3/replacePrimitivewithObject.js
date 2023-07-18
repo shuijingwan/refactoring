b4dcb9cdb53f5cb66c927a5c63bd6fb81dc01bd8
wangqiang <958104607@qq.com>
2023-07-18 21:28:03 +0800

    这样做的意义在于，现在新的Priority类可以容纳更多业务行为——无论是新的业务代码，还是从别处搬移过来的

--- 7/7.3/replacePrimitivewithObject.js
+++ 7/7.3/replacePrimitivewithObject.js
@@ -2,7 +2,7 @@
 
 orders.filter(o => o.priority.higherThan(new Priority("normal")))
 
-highPriorityCount = orders.filter(o => "high" === o.priority.toString() || "rush" === o.priority.toString()).length;
+highPriorityCount = orders.filter(o => o.priority.higherThan(new Priority("normal"))).length;
 
 class Order {
     constructor(data) {
@@ -22,10 +22,33 @@ class Order {
 class Priority {
     constructor(value) {
         if (value instanceof Priority) return value;
-        this._value = value;
+        if (Priority.legalValues().includes(value))
+            this._value = value;
+        else
+            throw new Error(`<${value}> is invalid for Priority`);
     }
 
     toString() {
         return this._value;
+    }
+
+    get _index() {
+        return Priority.legalValues().findIndex(s => s === this._value);
+    }
+
+    static legalValues() {
+        return ['low', 'normal', 'high', 'rush'];
+    }
+
+    equals(other) {
+        return this._index === other._index;
+    }
+
+    higherThan(other) {
+        return this._index > other._index;
+    }
+
+    lowerThan(other) {
+        return this._index < other._index;
     }
 }
