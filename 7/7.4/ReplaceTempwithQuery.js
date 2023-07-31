a3cdc776387e5b929e93c7ac62d32f17c5c233b6
wangqiang <958104607@qq.com>
2023-07-31 21:35:40 +0800

    以查询取代临时变量（Replace Temp with Query）

--- 7/7.4/ReplaceTempwithQuery.js
+++ 7/7.4/ReplaceTempwithQuery.js
@@ -1,5 +1,15 @@
-const basePrice = this._quantity * this._itemPrice;
-if (basePrice > 1000)
-    return basePrice * 0.95;
+// const basePrice = this._quantity * this._itemPrice;
+// if (basePrice > 1000)
+//     return basePrice * 0.95;
+// else
+//     return basePrice * 0.98;
+
+get basePrice()
+{
+    this._quantity * this._itemPrice;
+}
+
+if (this.basePrice > 1000)
+    return this.basePrice * 0.95;
 else
-    return basePrice * 0.98;
+    return this.basePrice * 0.98;
