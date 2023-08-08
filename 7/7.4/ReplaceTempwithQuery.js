2afcc1bb46ab60b2964d9edff91cd7fbb07af718
wangqiang <958104607@qq.com>
2023-08-08 21:37:03 +0800

    这里有一个简单的订单类

--- 7/7.4/ReplaceTempwithQuery.js
+++ 7/7.4/ReplaceTempwithQuery.js
@@ -4,12 +4,26 @@
 // else
 //     return basePrice * 0.98;
 
-get basePrice()
-{
-    this._quantity * this._itemPrice;
-}
+// get basePrice()
+// {
+//     this._quantity * this._itemPrice;
+// }
+//
+// if (this.basePrice > 1000)
+//     return this.basePrice * 0.95;
+// else
+//     return this.basePrice * 0.98;
 
-if (this.basePrice > 1000)
-    return this.basePrice * 0.95;
-else
-    return this.basePrice * 0.98;
+class Order {
+    constructor(quantity, item) {
+        this._quantity = quantity;
+        this._item = item;
+    }
+
+    get price() {
+        var basePrice = this._quantity * this._item.price;
+        var discountFactor = 0.98;
+        if (basePrice > 1000) discountFactor -= 0.03;
+        return basePrice * discountFactor;
+    }
+}
